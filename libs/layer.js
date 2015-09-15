9b8c4fa2836c7902b9c89e88e5ebb4dd77e698d1
hametan <1518981153@qq.com>
2015-09-15 10:14:52 +0800

    package.json

---
+++ libs/layer.js
@@ -0,0 +1,23 @@
+// 项目名必须与文件夹名一致
+{
+  "name": "layer",
+  "filename": "layer.js",
+  "version": "2.0",
+    "mobile": "1.6",
+  "description": "弹层组件",
+  "homepage": "http://layer.layui.com/",
+  "keywords": ["layer", "弹出层", "弹层"],
+  "maintainers": [
+	{
+	  "name": "贤心",
+	  "web": "http://layer.layui.com/",
+	  "mail": "852749347@qq.com"
+	}
+  ],
+  "repositories": [
+	{
+	  "type": "git",
+	  "url": "https://github.com/sentsin/layer"
+	}
+  ]
+}
